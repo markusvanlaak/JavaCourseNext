c08288761e5ec0db3d653960daf062745ca7700e
Markus van Laak <makusvanlaak@gmail.com>
2018-04-06 15:34:37 +0200

    Basic Learing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        // write your code here
+        int newVar;
+        newVar = 1;
+        //System.out.print(newVar);
+        while (newVar <= 10) {
+
+            System.out.println(newVar);
+            newVar++;
+        }
+        float myFloat = 11;
+        float modFloat;
+        modFloat = myFloat % 2;
+        System.out.print(modFloat);
+        if (modFloat > 0) {
+            System.out.println("If");
+        }
+        int newZahl = newVar > 1 ? 10 : 12;
+        System.out.print(newZahl);
     }
 }
